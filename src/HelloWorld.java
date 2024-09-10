ac7929b217e3b794184159d91d4920858371c5fc
natnardone <89878160+natnardone@users.noreply.github.com>
2024-09-09 21:26:11 -0500

    test message

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
   public static void main (String[] args) {
       PrintWriter pen = new PrintWriter(System.out, true);
       pen.println ("Hello, VSCode!");
+      pen.println("Test");
       pen.flush();
       pen.close();
     } // main(String[])
