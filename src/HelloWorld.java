02c3300b19a1b33f64952a3c349bbb23d7333876
Natalie Nardone <nardonen@grinnell.edu>
2024-09-09 21:25:31 -0500

    Change string

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -3,7 +3,7 @@ import java.io.PrintWriter;
 public class HelloWorld {
   public static void main (String[] args) {
       PrintWriter pen = new PrintWriter(System.out, true);
-      pen.println ("Hello, VSCode!");
+      pen.println ("Hi, VSCode!");
       pen.flush();
       pen.close();
     } // main(String[])
